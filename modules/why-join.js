718b8657cd18f5fc64229355b778c79ac78d8f9f
Akinkunmi <akinkunmiisrael41@gmail.com>
2021-01-26 18:45:29 +0000

    done for now!!!

--- modules/why-join.js
+++ modules/why-join.js
@@ -4,24 +4,24 @@ const whyJoin = () =>
     <h1>Why Join NewDev?</h1>
     <div class="row">
         <div class="content">
-            <div class="icon"></div>
+            <div class="icon"><i class="fas fa-project-diagram"></i></div>
             <h3>Connect with developers</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, cum? Remlabore, aliquam ab maiores sint natus repellendus enim quaerat.</p>
+            <p>Connect with developers around the world, discuss and share ideas and other fun and useful stuff.</p>
         </div>
         <div class="content">
-            <div class="icon"></div>
+            <div class="icon"><i class="fas fa-question"></i></div>
             <h3>Ask Questions</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, cum? Remlabore, aliquam ab maiores sint natus repellendus enim quaerat.</p>
+            <p>Got stuck? Need some help? Feel free to ask for help within our amazing community, we'll be more than glad to turn up for you.</p>
         </div>
         <div class="content">
-            <div class="icon"></div>
+            <div class="icon"><i class="fas fa-laptop-code"></i></div>
             <h3>Participate In Weekly Challenges</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, cum? Remlabore, aliquam ab maiores sint natus repellendus enim quaerat.</p>
+            <p>Participate in weekly challenges and get to compete with other developers, making you able to learn from others and also gives you the opportunity to get your code reviewed by the admin and also stand a chance to get a gift if you win. </p>
         </div>
         <div class="content">
-            <div class="icon"></div>
+            <div class="icon"><i class="fas fa-comment-dots"></i></div>
             <h3>Contribute</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, cum? Remlabore, aliquam ab maiores sint natus repellendus enim quaerat.</p>
+            <p>Have something to share with others? Feel free to share it, you would be helping a lot of developers and you might also stand a chance to be gifted something if you're a top-contributor.</p>
         </div>
     </div>
 </div>
